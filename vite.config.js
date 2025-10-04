17c80702863190c8a3a0496f49ce5c07b6f1a8cf
NeverstillHub <122246272+NeverstillHub@users.noreply.github.com>
2025-10-03 21:41:24 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Your GitHub Pages subpath:
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GHPAGES_BASE || '/',
+  base: '/bucketlisted-mocks/',   // <- hard-set to your repo name
 })
